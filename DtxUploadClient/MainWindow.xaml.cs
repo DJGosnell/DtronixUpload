cb53c5a3a8ac5adeecfea94ab9840ed5637d7684
DJGosnell <DJGosnell@gmail.com>
2014-12-15 09:18:29 -0500

    Renamed namespace for the client project to "DtxUpload". Added project to the SLN.

--- DtxUploadClient/MainWindow.xaml.cs
+++ DtxUploadClient/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DtxUploadClient {
+namespace DtxUpload {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
